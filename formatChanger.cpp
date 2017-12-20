3cc5d67f3f409e8932db9db55b74e2e601227c7e
BakedRibs <18101325879@163.com>
2017-12-20 15:14:47 +0800

    20171220-1514

--- formatChanger.cpp
+++ formatChanger.cpp
@@ -4,13 +4,25 @@ FormatChanger::FormatChanger()
 {
     openFileButton = new QPushButton("ChooseFile");
     changeFormatButton = new QPushButton("ChangeFormat");
-    filePathLabel = new QLabel();
+    changeFormatButton->setEnabled(false);
+    filePathLabel = new QLabel("Click \'ChooseFile\' to load txt file");
+    filePathLabel->setMinimumWidth(400);
+    filePathLabel->setAlignment(Qt::AlignHCenter);
+    filePath = "";
 
     QHBoxLayout *HLayout = new QHBoxLayout();
     HLayout->addWidget(openFileButton);
     HLayout->addWidget(changeFormatButton);
 
     QVBoxLayout *VLayout = new QVBoxLayout();
+    VLayout->addLayout(HLayout);
+    VLayout->addWidget(filePathLabel);
+
+    this->setLayout(VLayout);
+    this->show();
+
+    connect(openFileButton,SIGNAL(clicked()),this,SLOT(openFile()));
+    connect(changeFormatButton,SIGNAL(clicked()),this,SLOT(changeFormat()));
 }
 
 FormatChanger::~FormatChanger()
@@ -20,10 +32,25 @@ FormatChanger::~FormatChanger()
 
 void FormatChanger::openFile()
 {
-
+    filePath = QFileDialog::getOpenFileName(this, tr("Open Txt File"), ".", tr("Txt Files(*.txt)"));
+    if(filePath != "")
+    {
+        filePathLabel->setText(filePath);
+        changeFormatButton->setEnabled(true);
+    }
+    else
+    {
+        filePathLabel->setText("Click \'ChooseFile\' to load txt file");
+        changeFormatButton->setEnabled(false);
+    }
 }
 
 void FormatChanger::changeFormat()
+{
+
+}
+
+void FormatChanger::txtToXml()
 {
 
 }
